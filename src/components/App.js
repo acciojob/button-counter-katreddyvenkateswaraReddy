97a0e79fb962833315c64336b2b14ff58c84c044
katreddyvenkateswaraReddy <katreddy41@gmail.com>
2023-12-09 17:02:44 +0530

    new changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [count, setCount] = useState(0)
+
+    function increase(){
+        setCount(count+1)
+    }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick={increase}>Click me</button>
     </div>
   )
 }
